c2bd47a08c2362f8a60c5449ecc03ddbb91f7776
Motherbloods <habibskh06@gmail.com>
2024-09-06 21:48:58 +0700

    183. Customers Who Never Order

---
+++ 183. Customers Who Never Order.sql
@@ -0,0 +1,3 @@
+-- https://leetcode.com/problems/customers-who-never-order/description/
+
+select Customers.name as Customers from Customers LEFT JOIN Orders ON Customers.id = Orders.customerId WHERE Orders.customerId IS NULL;
