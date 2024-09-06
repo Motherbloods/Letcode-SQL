2c6e2a0f2e86644230460897dd78cdbff363187b
Motherbloods <habibskh06@gmail.com>
2024-09-06 23:04:45 +0700

    update on questions 176 and 182

--- 182. Duplicate Emails.sql
+++ 182. Duplicate Emails.sql
@@ -1,3 +1,3 @@
 --https://leetcode.com/problems/duplicate-emails/
 
-select email from Person group by email having count(email) > 1
+select DISTINCT email from Person group by email having count(email) > 1
